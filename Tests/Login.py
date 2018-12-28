f112d565cf608b8002a0fab8b78e97506b760d56
paultran1975 <45541034+paultran1975@users.noreply.github.com>
2018-12-28 11:30:18 -0800

    Initial commit

---
+++ Tests/Login.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+import time
+import unittest
+from Pages.HomePage import HomePage
+from Pages.LoginPage import Login
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)
+
+    def test_login_logout(self):
+        driver = self.driver
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ Util/Configs.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+__author__ = "Siraj Saleheen"
+
+import sys
+import pytest
+from selenium import webdriver
+
+@pytest.fixture(autouse=True, scope="class")
+def initialize_chrome_driver(request):
+    platform_darwin = "darwin"
+    platform_windows = "win32"
+    actual_platform = sys.platform
+    print("\n--------------------------------")
+    print("Platform:", actual_platform)
+    print("--------------------------------")
+    print("Initializing Chrome driver...")
+    print("--------------------------------")
+    if actual_platform in platform_darwin:
+        driver = webdriver.Chrome("./lib/drivers/chromedriver")
+    elif actual_platform in platform_windows:
+        driver = webdriver.Chrome("./lib/drivers/chromedriver.exe")
+    driver.get("https://opensource-demo.orangehrmlive.com")
+    driver.maximize_window()
+    request.cls.driver = driver
+
+    yield driver
+    driver.close()
